4c6b097a0949a30265932f3f194766e2a5ec273c
dimasm93 <software.dimas_m@icloud.com>
2022-02-02 12:18:28 +0700

    update draft

--- src/main/resources/db/migration/V2018_09_19_03_02_37__data-oauth-client-example.sql
+++ src/main/resources/db/migration/V2018_09_19_03_02_37__data-oauth-client-example.sql
@@ -1,23 +1,26 @@
 insert into resource.client_details(id, name, password, is_auto_approve, token_expired_in_second, created_by,
                                     created_date, last_update_by, last_update_date)
-values ('resource-postgresql96', 'resource-postgresql96',
+values ('sipening-auth', 'sipening-auth',
         '$2a$11$52ykJMuT3eooYzfYkZYIF.kZaStN4AGBCMI43aFoBC/Br5QcqHp3G', true, 43200, 'migration', now(), null, null),
-       ('oauth2-resource-oracle', 'oauth2-resource-oracle',
+       ('sipening-api', 'sipening-api',
         '$2a$11$52ykJMuT3eooYzfYkZYIF.kZaStN4AGBCMI43aFoBC/Br5QcqHp3G', true, 43200, 'migration', now(), null, null),
-       ('oauth2-resource-mysql', 'oauth2-resource-mysql',
+       ('sipening-webapp', 'sipening-webapp',
         '$2a$11$52ykJMuT3eooYzfYkZYIF.kZaStN4AGBCMI43aFoBC/Br5QcqHp3G', true, 43200, 'migration', now(), null, null),
-       ('oauth2-resource-mssql', 'oauth2-resource-mssql',
+       ('sipening-integration', 'sipening-integration',
+        '$2a$11$52ykJMuT3eooYzfYkZYIF.kZaStN4AGBCMI43aFoBC/Br5QcqHp3G', true, 43200, 'migration', now(), null, null),
+       ('sipening-report', 'sipening-report',
         '$2a$11$52ykJMuT3eooYzfYkZYIF.kZaStN4AGBCMI43aFoBC/Br5QcqHp3G', true, 43200, 'migration', now(), null, null);
 
 insert into resource.applications (id, name, created_by, created_date, last_update_by, last_update_date)
-values ('example', 'example', 'migration', now(), null, null);
+values ('sipening', 'Sistem Pengelolaan Rekening', 'migration', now(), null, null);
 
-insert into resource.client_detail_applications(id, client_detail_id, app_id, created_by, created_date, last_update_by, last_update_date)
-values
-       (uuid_generate_v4(), 'resource-postgresql96', 'example', 'migration', now(), null, null),
-       (uuid_generate_v4(), 'oauth2-resource-oracle', 'example', 'migration', now(), null, null),
-       (uuid_generate_v4(), 'oauth2-resource-mysql', 'example', 'migration', now(), null, null),
-       (uuid_generate_v4(), 'oauth2-resource-mssql', 'example', 'migration', now(), null, null);
+insert into resource.client_detail_applications(id, client_detail_id, app_id, created_by, created_date, last_update_by,
+                                                last_update_date)
+values (uuid_generate_v4(), 'sipening-auth', 'sipening', 'migration', now(), null, null),
+       (uuid_generate_v4(), 'sipening-api', 'sipening', 'migration', now(), null, null),
+       (uuid_generate_v4(), 'sipening-webapp', 'sipening', 'migration', now(), null, null),
+       (uuid_generate_v4(), 'sipening-report', 'sipening', 'migration', now(), null, null),
+       (uuid_generate_v4(), 'sipening-integration', 'sipening', 'migration', now(), null, null);
 
 insert into oauth.client_scopes(id, name, created_by, created_date, last_update_by, last_update_date)
 values (2, 'write', 'migration', now(), null, null),
--- src/main/resources/db/migration/V2019_06_28_14_47_10__data-menu-component.sql
+++ src/main/resources/db/migration/V2019_06_28_14_47_10__data-menu-component.sql
@@ -1,4 +1,4 @@
 insert into component.menu (id, title, path, icon_id, is_menu, created_by, created_date, module, parent_id)
-values ('001', 'Dashboard', '/dashboard', null, false, 'migration', now(), 'webapp', null),
-       ('002', 'Data Master', '/master', null, true, 'migration', now(), 'webapp', null),
-       ('003', 'Data Example', '/example', null, false, 'migration', now(), 'webapp', '002');
+values ('001', 'Dashboard', '/dashboard', null, false, 'migration', now(), 'sipening-webapp', null),
+       ('002', 'Data Master', '/master', null, true, 'migration', now(), 'sipening-webapp', null),
+       ('003', 'Data Example', '/example', null, false, 'migration', now(), 'sipening-webapp', '002');
